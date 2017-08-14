23427ae7dc5e68557f59cf083d47b78ff489fe94
JaylonQi@outlook.com <JaylonQi@outlook.com>
2017-08-14 10:48:46 +0800

    good

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ const handle = app.getRequestHandler()
 app.prepare()
 .then(() => {
   const server = express()
-
+  server.get('/p/:id', (req, res) => {
+    const actualPage = '/post'
+    const queryParams = { id: req.params.id } 
+    app.render(req, res, actualPage, queryParams)
+  })
   server.get('*', (req, res) => {
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(3004, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:3004')
   })
 })
 .catch((ex) => {
